8ed3c93fae49a946aaa7db583666a99afc2dac99
liulin <lynnwonder@163.com>
2019-11-05 20:59:02 +0800

    add node

--- node/test_http/02_server.js
+++ node/test_http/02_server.js
@@ -10,6 +10,7 @@ const fs=require('fs');
 //  /test 只用write 不用end  =>  默认来讲 页面会一直转
 http.createServer((req,res)=>{
     if (req.url==='/'){
+        // 这是开发中非常常见的：同步请求，响应页面。
         fs.readFile('./index.html',(err,data)=>{
             res.writeHead(200,{'content-type':'text/html;charset=utf-8'});
             res.end(data);
