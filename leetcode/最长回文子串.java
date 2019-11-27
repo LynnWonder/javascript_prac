764bc932f36d876094dd234ca7a1b8bb3595975b
liulin <lynnwonder@163.com>
2019-11-27 21:48:39 +0800

    add leetcode-5

---
+++ leetcode/最长回文子串.java
@@ -0,0 +1,17 @@
+public String longestPalindrome(String s) {
+    int length = s.length();
+    boolean[][] P = new boolean[length][length];
+    int maxLen = 0;
+    String maxPal = "";
+    for (int len = 1; len <= length; len++) //遍历所有的长度
+        for (int start = 0; start < length; start++) {
+            int end = start + len - 1;
+            if (end >= length) //下标已经越界，结束本次循环
+                break;
+            P[start][end] = (len == 1 || len == 2 || P[start + 1][end - 1]) && s.charAt(start) == s.charAt(end); //长度为 1 和 2 的单独判断下
+            if (P[start][end] && len > maxLen) {
+                maxPal = s.substring(start, end + 1);
+            }
+        }
+    return maxPal;
+}
