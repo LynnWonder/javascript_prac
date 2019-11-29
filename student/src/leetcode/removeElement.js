e500d9ba449bd43323be0316b163e85f6f7c76c6
mingliang <23897631@qq.com>
2019-11-29 23:40:42 +0800

    code style

--- student/src/leetcode/removeElement.js
+++ student/src/leetcode/removeElement.js
@@ -1,5 +1,4 @@
-/* 
-给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+/* 给定一个数组nums和一个值val，你需要原地移除所有数值等于val的元素，返回移除后数组的新长度。
 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
 
@@ -8,7 +7,7 @@
 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
 你不需要考虑数组中超出新长度后面的元素。
 
-示例 2:
+示例2:
 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
 注意这五个元素可为任意顺序。
@@ -25,14 +24,14 @@ int len = removeElement(nums, val);
 // 在函数里修改输入数组对于调用者是可见的。
 // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
 for (int i = 0; i < len; i++) {
-    print(nums[i]);
+ print(nums[i]);
 }
 
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/remove-element
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
 
-const removeElement = (nums, val) => {
+export const removeElement = (nums, val) => {
   const loop = (arr) => {
     const index = arr.indexOf(val)
     if (index > -1) {
