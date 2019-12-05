2094cb325fa288f31e898adca4f89601463b8e86
liulin <lynnwonder@163.com>
2019-12-05 15:11:52 +0800

    add node-student-list-express

--- node/test_express/06_student_list/app.js
+++ node/test_express/06_student_list/app.js
@@ -1,19 +1,10 @@
 let express=require('express');
 let path=require('path');
+let formidable=require('formidable');
 let app=express();
 let router=express.Router();
 
-
-let heros=[
-    {
-        name:'purple hair',
-        img:'imgs/1.jpg'
-    },
-    {
-        name:'black hair',
-        img:'imgs/2.jpg'
-    }
-]
+let heros=[];
 
 app.engine('.html',require('express-art-template'));
 app.set('view options', {
@@ -25,6 +16,36 @@ router.get('/',(req,res,next)=>{
     // es6简写
     res.render('index',{heros});
 })
+    .post('/add',(req,res,next)=>{
+        let form = new formidable.IncomingForm();
+        // change upload dir
+        form.uploadDir = path.join(__dirname,'public','imgs');
+        // keep extensions
+        form.keepExtensions=true;
+        console.info(form);
+        form.parse(req, function(err, fields, files) {
+            // res.writeHead(200, {'content-type': 'text/plain'});
+            // res.write('received upload:\n\n');
+            // res.end(util.inspect({fields: fields, files: files}));
+            // console.info(fields);
+            // console.info(files);
+            // fields.nickname
+            // files.avatar.path
+            // get dir's filename ==>path.basename(dir)
+            // then we change the object:hero;
+            let nickname=fields.nickname;
+            // console.info(path.basename(files.avatar.path,'.jpg'));
+            let filename=path.parse(files.avatar.path).base;
+            let img='imgs/'+filename;
+            heros.push({
+                name:nickname,
+                img
+            });
+            // 同步提交，浏览器等待页面显示
+            res.redirect('/');
+        });
+    })
+    //最后一条路由中
 .all('*',(req,res)=>{
     res.send('bad request');
 });
