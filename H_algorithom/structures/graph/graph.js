9a6afa6d7727079095636a6e2183f05d8bb263c2
liulin <lynnwonder@163.com>
2019-12-14 16:19:34 +0800

    add checkDAGvsDCG and topologicalSort

--- H_algorithom/structures/graph/graph.js
+++ H_algorithom/structures/graph/graph.js
@@ -87,6 +87,82 @@ function findMinPath(graph,st,ed){
     res.unshift(st);
     return res;
 }
+
+/**
+ * 检查有向图中是否有环检查是DAG(directed acyclic graph) VS DCG(directed cyclic graph)
+ * dfs与标记「正在访问」检查法
+ */
+const checkDAG=(arr,num)=>{
+    let adj=new Array(num),visited=new Array(num).fill(false),
+        being_visited=new Array(num).fill(false);
+    // initialize
+    for(let i=0;i<num;i++){
+        adj[i]=[];
+    }
+    for(let i=0;i<arr.length;i++){
+        adj[arr[i][0]].push(arr[i][1]);
+    }
+    const dfs=(v,being_visited,visited)=>{
+        // 如果dfs时当前顶点正在被访问，那么必定有环
+        if(being_visited[v]){
+            console.info('cycle==>',v);
+            return false;
+        }
+        // 顶点并没有正在被访问，但是访问过了，那么略过
+        if(visited[v]) return true;
+        being_visited[v]=true;
+        visited[v]=true;
+        for(let i=0;i<adj[v].length;i++){
+            if(!dfs(adj[v][i],being_visited,visited)) return false;
+        }
+        // 正常退场
+        being_visited[v]=false;
+        return true;
+    };
+    // 检查每一个顶点的下个顶点是否有环
+    for(let i=0;i<num;i++){
+        if(visited[i]) continue;
+        if(!dfs(i,being_visited,visited)) return false;
+    }
+    return true;
+};
+/**
+ * 拓扑排序
+ * @param arr
+ * @param num
+ */
+const topoSort=(arr,num)=>{
+    let res=[],adj=new Array(num),inDegree=new Array(num).fill(0),temp=[];
+    let count=0;
+    // initialize
+    for(let i=0;i<num;i++){
+        adj[i]=[];
+    }
+    for(let i=0;i<arr.length;i++){
+        adj[arr[i][0]].push(arr[i][1]);
+        inDegree[arr[i][1]]+=1;
+    }
+    // collect which inDegree's 0
+    for(let i=0;i<num;i++){
+        if(inDegree[i]===0){
+            temp.push(i);
+        }
+    }
+    while(temp.length>0){
+        let top=temp.pop();
+        count++;
+        res.push(top);
+        for(let i=0;i<adj[top].length;i++){
+            inDegree[adj[top][i]]-=1;
+            if(inDegree[adj[top][i]]===0){
+                temp.push(adj[top][i]);
+            }
+        }
+    }
+    console.info(res);
+    return count===num;
+};
+
 let g=new Graph(6);
 g.addEdge(0,1);
 g.addEdge(0,4);
@@ -94,12 +170,21 @@ g.addEdge(1,2);
 g.addEdge(2,3);
 g.addEdge(4,5);
 g.addEdge(5,3);
-console.info(g.adj);
-console.info(DFSearch(g,0));
-console.info(BFSearch(g,0));
+let arr0=[
+    [0,1],
+    [1,2],
+    [2,3],
+    [2,1]
+];
+// // console.info(g.adj);
+// console.info(DFSearch(g,0));
+// console.info(BFSearch(g,0));
+// console.info(checkDAG(arr0,4));
+console.info(topoSort(arr0,4));
 export default {
     Graph,
     DFSearch,
     BFSearch,
-    findMinPath
+    findMinPath,
+    checkDAG
 };
