4cf1589db07997926beef2eedb8ea10ffcc42e60
liulin <lynnwonder@163.com>
2020-03-02 10:46:32 +0800

    leetcode-55-opt

--- leetcode/jump-game.js
+++ leetcode/jump-game.js
@@ -11,6 +11,7 @@
  * 状态转移方程F(n)=(F(n-1)&&arr[n-1]>=1)||(F(n-1)&&arr[n-2]>=2)
  * 最优子结构 arr[x]
  * 边界条件  F(1)=true
+ * 时间复杂度：O(n^2) 空间复杂度：O(n)
  * @param {number[]} nums
  * @return {boolean}
  */
@@ -21,11 +22,31 @@ const canJump = (nums)=>{
         let flag=false;
         for(let j=0;j<i;j++){
             flag=flag||(temp[j]&&nums[j]>=(i-j));
+            // opt1:既然是取或操作，若为真命题只需要检查一次就可以
+            if(flag===true) break;
         }
         temp[i]=flag;
     }
     // console.info(temp);
     return temp[nums.length-1];
 };
+/**
+ * 改进后的贪心算法
+ * 可以从最后一个元素开始遍历，而且只需要遍历一遍即可
+ * 不停的向前缩进确定最早一个能到达重点的坐标
+ * @param nums
+ * @returns {boolean}
+ */
+const canJump1=nums=>{
+    let lastPos=nums.length-1;
+    for(let i=nums.length-1;i>=0;i--){
+        if(i+nums[i]>=lastPos){
+            lastPos=i;
+        }
+    }
+    return lastPos===0;
+};
 console.info(canJump([2,3,1,1,4]));
+console.info(canJump1([2,3,1,1,4]));
 console.info(canJump([3,2,1,0,4]));
+console.info(canJump1([3,2,1,0,4]));
